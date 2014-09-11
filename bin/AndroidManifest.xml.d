0c600dd76e3f7a01a0ecb1a1469ad6df445050d7
TruongSinh Tran-Nguyen <i@truongsinh.pro>
2014-09-11 14:32:08 +0300

    init

---
+++ bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\DuyImba\Documents\smarpionic\platforms\android\FacebookLib\bin\AndroidManifest.xml : \
+C:\Users\DuyImba\Documents\smarpionic\platforms\android\FacebookLib\AndroidManifest.xml \
